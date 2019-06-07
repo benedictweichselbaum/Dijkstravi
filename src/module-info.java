c56482fe93609bc72a0dbe1afd1ab0a87e45a41f
benedictweichselbaum <bmw00178@gmail.com>
2019-06-07 17:34:40 +0200

    inital commit

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+module Dijkstravi {
+
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    requires javafx.swt;
+
+    opens application;
+}
