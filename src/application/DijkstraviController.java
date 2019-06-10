64a82aceb5b33d685f8a90d7f43cfd5250a09797
benedictweichselbaum <bmw00178@gmail.com>
2019-06-10 14:31:39 +0200

    First GUI with Controller Class

---
+++ src/application/DijkstraviController.java
@@ -0,0 +1,69 @@
+package application;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+public class DijkstraviController implements Initializable {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnCalcRoute;
+
+    @FXML
+    private RadioButton rbAStrern;
+
+    @FXML
+    private RadioButton rbBellmanFord;
+
+    @FXML
+    private RadioButton rbDijkstra;
+
+    @FXML
+    private RadioButton rbMinPlusMma;
+
+    @FXML
+    private TextArea txtAreaRoute;
+
+    @FXML
+    private TextField txtFrom;
+
+    @FXML
+    private TextField txtTo;
+
+    @FXML
+    private ImageView imgViewAutobahn;
+
+    private ToggleGroup algRadioButtonGroup;
+
+    @FXML
+    void clickedCalcRoute(ActionEvent event) {
+        txtAreaRoute.setText("Hello, you just have pressed a Button.\nYou are Great!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        algRadioButtonGroup = new ToggleGroup();
+        rbAStrern.setToggleGroup(algRadioButtonGroup);
+        rbBellmanFord.setToggleGroup(algRadioButtonGroup);
+        rbDijkstra.setToggleGroup(algRadioButtonGroup);
+        rbMinPlusMma.setToggleGroup(algRadioButtonGroup);
+
+        File file = new File("src/autobahnnetz_DE.png");
+        Image image = new Image(file.toURI().toString());
+        imgViewAutobahn.setImage(image);
+    }
+}
+
