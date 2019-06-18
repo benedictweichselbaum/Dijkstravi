67b07ef51f28e3607dea3fde65bd251cbe6d78f5
dtiggy <Warum567?>
2019-06-18 11:17:06 +0200

    JDOMTest

---
+++ src/application/xmlParser/JDOMTest.java
@@ -0,0 +1,29 @@
+package application.xmlParser;
+import java.io.*;
+import java.util.*;
+import org.jdom2.*;
+
+import org.jdom2.Attribute;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+public class JDOMTest {
+
+    public static void main(String[] args) {
+
+        try {
+            File inputFile = new File("../german_autobahn.osm");
+            SAXBuilder saxBuilder = new SAXBuilder();
+            Document document = saxBuilder.build(inputFile);
+            System.out.println("Root element :" + document.getRootElement().getName());
+            //   Element classElement = document.getRootElement();
+
+        } catch(JDOMException e) {
+            e.printStackTrace();
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
