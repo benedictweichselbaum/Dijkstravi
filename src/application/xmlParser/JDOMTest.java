9dbf0caa0961340cb4bfb85d88304c061303aec5
dtiggy <Warum567?>
2019-06-18 11:53:18 +0200

    JDOMTest: Anzahl der Objekte werden gezählt

--- src/application/xmlParser/JDOMTest.java
+++ src/application/xmlParser/JDOMTest.java
@@ -18,7 +18,34 @@ public class JDOMTest {
             SAXBuilder saxBuilder = new SAXBuilder();
             Document document = saxBuilder.build(inputFile);
             System.out.println("Root element :" + document.getRootElement().getName());
-            //   Element classElement = document.getRootElement();
+            Element classElement = document.getRootElement();
+
+            List<Element> wayList = classElement.getChildren();
+            System.out.println("----------------------------");
+
+            System.out.println("Anzahl Wegpunkte:" + wayList.size());
+
+            System.out.println("----------------------------");
+            System.out.println("Erste 10 Einträge");
+
+            int ways = 0;
+            int nodes = 0;
+            int others = 0;
+
+            for (int i = 0; i < wayList.size(); i++) {
+                Element way = wayList.get(i);
+                //System.out.println("\nAktuelles Element :"
+                       // + way.getName());
+                if(way.getName().equals("way"))
+                    ways++;
+                else if(way.getName().equals("node"))
+                    nodes++;
+                else
+                    others++;
+            }
+            System.out.println("Wege: " + ways + " Knoten: " + nodes + " Andere: " + others);
+
+
 
         } catch(JDOMException e) {
             e.printStackTrace();
