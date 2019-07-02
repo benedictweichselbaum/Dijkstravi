73fd157bdfcd12439d2e77ca88d21eb051a5bc8f
dtiggy <Warum567?>
2019-07-02 22:13:23 +0200

    Optimized zoomer

--- src/application/imageManipulation/Zoomer.java
+++ src/application/imageManipulation/Zoomer.java
@@ -1,17 +1,10 @@
 package application.imageManipulation;
 
-import javafx.application.Application;
-import javafx.beans.InvalidationListener;
-import javafx.beans.Observable;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.event.Event;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.ScrollEvent;
-import javafx.stage.Stage;
 
 
 public class Zoomer{
@@ -24,34 +17,23 @@ public class Zoomer{
         this.imageView = imgView;
         this.scrollPane = scrollPane;
 
-        zoomProperty.addListener(new InvalidationListener() {
-            @Override
-            public void invalidated(Observable arg0) {
-                imageView.setFitWidth(zoomProperty.get() * 4);
-                imageView.setFitHeight(zoomProperty.get() * 3);
+        zoomProperty.addListener(arg0 -> {
+            imageView.setFitWidth(zoomProperty.get() * 4);
+            imageView.setFitHeight(zoomProperty.get() * 3);
+        });
+
+        imageView.addEventFilter(ScrollEvent.ANY, event -> {
+            if (event.getDeltaY() > 0) {
+                zoomProperty.set(zoomProperty.get() * 1.1);
+            } else if (event.getDeltaY() < 0 && imageView.fitWidthProperty().getValue() > 605) {
+                zoomProperty.set(zoomProperty.get() / 1.1);
             }
         });
 
-        imageView.addEventFilter(ScrollEvent.ANY, new EventHandler<>() {
-            @Override
-            public void handle(ScrollEvent event) {
-                //System.out.println("Breite: " + imageView.fitWidthProperty().getValue());
-                if (event.getDeltaY() > 0) {
-                    zoomProperty.set(zoomProperty.get() * 1.1);
-                } else if (event.getDeltaY() < 0 && imageView.fitWidthProperty().getValue() > 605) {
-                    zoomProperty.set(zoomProperty.get() / 1.1);
-                }
-            }
+        scrollPane.addEventFilter(ScrollEvent.ANY, scrollEvent -> {
+            //System.out.println(scrollPane.getTranslateX() + "<T  >" + scrollPane.getViewportBounds().toString());
         });
 
-        scrollPane.addEventFilter(ScrollEvent.ANY, new EventHandler<>() {
-            @Override
-            public void handle(ScrollEvent scrollEvent) {
-                //System.out.println(scrollPane.getTranslateX() + "<T  >" + scrollPane.getViewportBounds().toString());
-            }
-        });
-
-        scrollPane.setDisable(true);
 /* Noch nicht löschen
         scrollPane.addEventFilter(Event.ANY, new EventHandler<Event>() {
             @Override
