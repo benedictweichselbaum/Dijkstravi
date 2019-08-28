9733c79b77e1eb81d9680064ce2ef7158a07ca4d
dtiggy <Warum567?>
2019-08-28 23:00:24 +0200

    emergency fix ;-)

--- src/application/graphNavigation/algorithms/NavigationService.java
+++ src/application/graphNavigation/algorithms/NavigationService.java
@@ -13,11 +13,16 @@ public abstract class NavigationService {
     int INFINITE = Integer.MAX_VALUE;
     ArrayList<Integer> dist;
     ArrayList<Integer> predecessor;
-    boolean fastestPath;
+    boolean fastestPath = true;
     int personalMaxSpeed;
 
     Double progress = 0.0;
 
+    public Stack<Integer> initCalculateShortestWay(Graph g, int startNode, int targetNode, int maxSpeed, boolean fastestPath){
+        this.fastestPath = fastestPath;
+        this.personalMaxSpeed = maxSpeed;
+        return calculateShortestWay(g, startNode, targetNode);
+    }
     public abstract Stack<Integer> calculateShortestWay(Graph g, int startNode, int targetNode);
 
     String directions(Graph g, Stack<Integer> way, int maxSpeed, boolean fastestPath) {
@@ -53,7 +58,10 @@ public abstract class NavigationService {
     }
 
     int getDistance(Connection con){
+
+       // System.out.println(fastestPath + "gD fP");
         if(fastestPath) {
+            //System.out.println(Math.round(MathematicOperations.calculateTimeForConnection(con, personalMaxSpeed)) + " " + personalMaxSpeed);
             return (int) Math.round(MathematicOperations.calculateTimeForConnection(con, personalMaxSpeed));
         }
         else
