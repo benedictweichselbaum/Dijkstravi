ee4cb28dadfca17c59bb355b92f3ab2478e11acb
dtiggy <Warum567?>
2019-08-30 21:02:45 +0200

    small UI-Function fixes

--- src/application/graphNavigation/algorithms/AlgorithmThread.java
+++ src/application/graphNavigation/algorithms/AlgorithmThread.java
@@ -139,7 +139,7 @@ public class AlgorithmThread extends Thread {
         controller.getTxtAreaRoute().setText("Es konnte keine Route von " + fromStr + " nach " + toStr + " gefunden werden.");
         this.abstractAlgorithm.progress = 1.0;
         controller.enableFields();
-
+        controller.deleteDistanceAndDuration();
         File imageFile = new File("src/application/autobahnnetz_DE.png");
         Image autobahnNetworkImage = new Image(imageFile.toURI().toString());
         controller.getImgViewAutobahn()
