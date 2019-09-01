5ad76119b16d6e93acb4a8c1c3ab4ccaee75e419
dominic0df <fischer.df@web.de>
2019-09-01 19:58:08 +0200

    MinPlus final Refactoring

--- src/application/graphNavigation/algorithms/MinPlusMatrixMultiplication.java
+++ src/application/graphNavigation/algorithms/MinPlusMatrixMultiplication.java
@@ -4,17 +4,22 @@ package application.graphNavigation.algorithms;
 import application.graphNavigation.graph.Connection;
 import application.graphNavigation.graph.Graph;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
 import java.util.stream.Collectors;
-
-
 
 /*
  * the structure of the algorithm is from the book
  * Introduction to Algorithms by Thomas H. Cormen, Charles Leiserson, Ronald L. Rivest, Clifford Stein
  * 4. Auflage. ISBN 978-3-486-74861-1, S. 697-704, german version
+ *
+ * The result matrix with all shortest distances is returned by the allPairsShortestPaths method.
+ * It is not used here, but characterises the algorithm.
+ * Other essential methods are extendShortestPaths and output
+ * For each Node the way to each other Node is saved as a list in the precursors list.
  */
-
 
 public class MinPlusMatrixMultiplication extends AbstractAlgorithm {
 
@@ -24,79 +29,16 @@ public class MinPlusMatrixMultiplication extends AbstractAlgorithm {
     @Override
     public Stack<Integer> calculateShortestWay(Graph g, int startNode, int targetNode) {
         numberOfNodes = g.getAmountOfNodes();
-        initArrays(numberOfNodes);
-        List<List<Integer>> graphAsMatrix = new ArrayList<>(numberOfNodes);
+        initPrecursorArray(numberOfNodes);
+        List<List<Integer>> graphAsMatrix = convertGraphAsAdjacencyListToAdjacencyMatrix(g);
+        fillPrecursorsArrayWithDirectConnections(graphAsMatrix);
 
-        for (int i = 0; i < numberOfNodes; i++) {
-            List<Integer> distanceOfNodesToOthers = new ArrayList<>(numberOfNodes);
-            for(int j= 0; j<numberOfNodes; j++){
-                distanceOfNodesToOthers.add(INFINITE);
-            }
-            ArrayList<Connection> allConnectionsOfNode = g.getAllConnectionsOfNode(i);
-            for (Connection connectionToNeighbor : allConnectionsOfNode) {
-                int aim = connectionToNeighbor.getAim();
-                distanceOfNodesToOthers.set(aim, getDistance(connectionToNeighbor));
-            }
-            graphAsMatrix.add(distanceOfNodesToOthers);
-        }
-        for (int i = 0; i < numberOfNodes; i++) {
-            for (int j = 0; j < numberOfNodes; j++) {
-                if (graphAsMatrix.get(i).get(j) != INFINITE) {
-                    precursors.get(i).set(j, i);
-                }
-                if (i == j) {
-                    precursors.get(i).set(j, INFINITE);
-                }
-            }
-        }
         List<List<Integer>> resultMatrix = allPairsShortestPaths(graphAsMatrix);
         return output(g, startNode, targetNode);
     }
 
-    private List<List<Integer>> extendShortestPaths(List<List<Integer>> calculatedMatrix, List<List<Integer>> inputMatrix) {
-        List<List<Integer>> shortestDistMatrix = new ArrayList<>(inputMatrix.stream().map(x -> new ArrayList<>(x)).collect(Collectors.toList()));
-        for (int i = 0; i < numberOfNodes; i++) {
-            List<Integer> tempList = new ArrayList<>(numberOfNodes);
-            for (int j = 0; j < numberOfNodes; j++) {
-                tempList.add(INFINITE);
-            }
-            shortestDistMatrix.add(tempList);
-        }
-
-        for (int i = 0; i < numberOfNodes; i++) {
-            for (int j = 0; j < numberOfNodes; j++) {
-                for (int k = 0; k < numberOfNodes; k++) {
-                    int oldValue = shortestDistMatrix.get(i).get(j);
-                    int newWeightPartOne = calculatedMatrix.get(i).get(k);
-                    int newWeightPartTwo = inputMatrix.get(k).get(j);
-
-                    if(newWeightPartOne < INFINITE && newWeightPartTwo < INFINITE && newWeightPartOne + newWeightPartTwo <= oldValue){
-                        shortestDistMatrix.get(i).set(j, newWeightPartOne + newWeightPartTwo);
-                        precursors.get(i).set(j, k);
-                    }
-                }
-            }
-        }
-        return shortestDistMatrix;
-    }
-
-
-    private List<List<Integer>> fasterAllPairsShortestPaths(List<List<Integer>> inputMatrix) {
-        List<List<Integer>> startMatrix = new ArrayList<>(inputMatrix.stream().map(x -> new ArrayList<>(x)).collect(Collectors.toList()));
-        List<List<List<Integer>>> matrices = new ArrayList<>(numberOfNodes);
-        matrices.add(startMatrix);
-        int m = 1;
-        while (m < numberOfNodes) {
-            List<List<Integer>> curMatrix = extendShortestPaths(matrices.get(m - 1), matrices.get(m - 1));
-            int indexInMatrices=2 * m - 1;
-            matrices.add(indexInMatrices, curMatrix);
-            m = 2 * m;
-        }
-        return matrices.get(matrices.size() - 1);
-    }
-
     private List<List<Integer>> allPairsShortestPaths(List<List<Integer>> inputMatrix) {
-        List<List<Integer>> startMatrix = new ArrayList<>(inputMatrix.stream().map(x -> new ArrayList<>(x)).collect(Collectors.toList()));
+        List<List<Integer>> startMatrix = new ArrayList<>(inputMatrix.stream().map(ArrayList::new).collect(Collectors.toList()));
         List<List<List<Integer>>> matrices = new LinkedList<>();
         matrices.add(startMatrix);
 
@@ -114,37 +56,40 @@ public class MinPlusMatrixMultiplication extends AbstractAlgorithm {
         return matrices.get(matrices.size() - 1);
     }
 
-    /*private void printOutMatrix(int[][] matrix) {
-        int width = 8;
-        int numberOfAddedNodes = matrix.length;
-        System.out.print("        ");
-        for (int i = 0; i < matrix.length; i++)
-            System.out.print(i + "        ");
-
-        System.out.println();
-
-        for (int i = 0; i < numberOfAddedNodes; i++) {
-            System.out.print(i + "       ");
-            for (int j = 0; j < numberOfAddedNodes; j++)
-                if (matrix[i][j] != -1)
-                    System.out.print((matrix[i][j] + "       ").substring(0, width));
-                else
-                    System.out.print("        ");
-            System.out.println();
+    //runtime improvement that is sadly not running at the moment
+    private List<List<Integer>> fasterAllPairsShortestPaths(List<List<Integer>> inputMatrix) {
+        List<List<Integer>> startMatrix = new ArrayList<>(inputMatrix.stream().map(ArrayList::new).collect(Collectors.toList()));
+        List<List<List<Integer>>> matrices = new ArrayList<>(numberOfNodes);
+        matrices.add(startMatrix);
+        int m = 1;
+        while (m < numberOfNodes) {
+            List<List<Integer>> curMatrix = extendShortestPaths(matrices.get(m - 1), matrices.get(m - 1));
+            int indexInMatrices=2 * m - 1;
+            matrices.add(indexInMatrices, curMatrix);
+            m = 2 * m;
         }
-    }*/
+        return matrices.get(matrices.size() - 1);
+    }
 
-    void initArrays(int amountOfNodes) {
-        precursors = new ArrayList<>(amountOfNodes);
+    private List<List<Integer>> extendShortestPaths(List<List<Integer>> calculatedMatrix, List<List<Integer>> inputMatrix) {
+        List<List<Integer>> shortestDistMatrix = initializeShortestDistMatrix(inputMatrix);
 
-        for (int i = 0; i < amountOfNodes; i++) {
-            List<Integer> precursorsOfNode = new ArrayList<>(amountOfNodes);
-            for (int j = 0; j < amountOfNodes; j++) {
-                precursorsOfNode.add(INFINITE);
+        for (int i = 0; i < numberOfNodes; i++) {
+            for (int j = 0; j < numberOfNodes; j++) {
+                for (int k = 0; k < numberOfNodes; k++) {
+                    int oldValue = shortestDistMatrix.get(i).get(j);
+                    int newWeightPartOne = calculatedMatrix.get(i).get(k);
+                    int newWeightPartTwo = inputMatrix.get(k).get(j);
+
+                    if(newWeightPartOne < INFINITE && newWeightPartTwo < INFINITE
+                            && newWeightPartOne + newWeightPartTwo <= oldValue){
+                        shortestDistMatrix.get(i).set(j, newWeightPartOne + newWeightPartTwo);
+                        precursors.get(i).set(j, k);
+                    }
+                }
             }
-            precursorsOfNode.set(i, 0);
-            precursors.add(precursorsOfNode);
         }
+        return shortestDistMatrix;
     }
 
     private Stack<Integer> output(Graph g, int startNode, int targetNode) {
@@ -167,5 +112,61 @@ public class MinPlusMatrixMultiplication extends AbstractAlgorithm {
             return way;
         }
     }
+
+    private void fillPrecursorsArrayWithDirectConnections(List<List<Integer>> graphAsMatrix) {
+        for (int i = 0; i < numberOfNodes; i++) {
+            for (int j = 0; j < numberOfNodes; j++) {
+                if (graphAsMatrix.get(i).get(j) != INFINITE) {
+                    precursors.get(i).set(j, i);
+                }
+                if (i == j) {
+                    precursors.get(i).set(j, INFINITE);
+                }
+            }
+        }
+    }
+
+    private List<List<Integer>> convertGraphAsAdjacencyListToAdjacencyMatrix(Graph g) {
+        List<List<Integer>> graphAsMatrix = new ArrayList<>(numberOfNodes);
+        for (int i = 0; i < numberOfNodes; i++) {
+            List<Integer> distanceOfNodesToOthers = new ArrayList<>(numberOfNodes);
+            for(int j= 0; j<numberOfNodes; j++){
+                distanceOfNodesToOthers.add(INFINITE);
+            }
+            ArrayList<Connection> allConnectionsOfNode = g.getAllConnectionsOfNode(i);
+            for (Connection connectionToNeighbor : allConnectionsOfNode) {
+                int aim = connectionToNeighbor.getAim();
+                distanceOfNodesToOthers.set(aim, getDistance(connectionToNeighbor));
+            }
+            graphAsMatrix.add(distanceOfNodesToOthers);
+        }
+        return graphAsMatrix;
+    }
+
+    private List<List<Integer>> initializeShortestDistMatrix(List<List<Integer>> inputMatrix) {
+        List<List<Integer>> shortestDistMatrix = new ArrayList<>(inputMatrix.stream().map(ArrayList::new).collect(Collectors.toList()));
+        for (int i = 0; i < numberOfNodes; i++) {
+            List<Integer> tempList = new ArrayList<>(numberOfNodes);
+            for (int j = 0; j < numberOfNodes; j++) {
+                tempList.add(INFINITE);
+            }
+            shortestDistMatrix.add(tempList);
+        }
+        return shortestDistMatrix;
+    }
+
+    private void initPrecursorArray(int amountOfNodes) {
+        precursors = new ArrayList<>(amountOfNodes);
+
+        for (int i = 0; i < amountOfNodes; i++) {
+            List<Integer> precursorsOfNode = new ArrayList<>(amountOfNodes);
+            for (int j = 0; j < amountOfNodes; j++) {
+                precursorsOfNode.add(INFINITE);
+            }
+            precursorsOfNode.set(i, 0);
+            precursors.add(precursorsOfNode);
+        }
+    }
+
 }
 
